0d7912564f45c93540d4fc29c6ecee8696c0f1f1
hn185036 <hn185036@ncr.com>
2021-02-18 10:26:24 +0800

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -25,7 +25,7 @@ namespace ConsoleApplication1
 
         private void ReadLineKey()
         {
-            Console.Writeline("Updated to Read Key master");
+            Console.Writeline("Updated to Read Key to new Source Forks");
         }
     }
 }
